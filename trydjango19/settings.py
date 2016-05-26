79af16ae62dad36a4cb56365cb612e8035e239ea
jek10 <jekalviar10@gmail.com>
2016-05-26 14:28:19 +0800

    Fixing css

--- trydjango19/settings.py
+++ trydjango19/settings.py
@@ -13,9 +13,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#nx7tuctv4=$_r$p7^h)2(m_j^p(d)(40ip=%h1%x6zs1k8jwt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
+
+
 
 
 # Application definition
